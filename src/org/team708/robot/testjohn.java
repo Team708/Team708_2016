0ffe92b9edf8915145f5c874b2a9947baaa46564
jpiergal <john.piergallini@lmco.com>
2016-01-30 14:55:07 -0500

    removed testjohn.java

--- src/org/team708/robot/testjohn.java
+++
@@ -1,7 +0,0 @@
-///test file - testing git
-
-//test2 on Wed
-// added a line 12:30 on Sat 23rd
-//one more change from laptop to master to pull down to mac
-// sue added newline at 1/25 at 714
-// sue created a branch and this is the 1st file being changed in the branch
