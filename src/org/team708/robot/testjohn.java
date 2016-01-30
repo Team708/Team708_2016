8b87e54a55034af0917867e7548cd5dd0ffebc16
jpiergal <john.piergallini@lmco.com>
2016-01-30 08:22:38 -0500

    git ignore jar and xml files

--- src/org/team708/robot/testjohn.java
+++ src/org/team708/robot/testjohn.java
@@ -3,6 +3,10 @@
 //test2 on Wed
 // added a line 12:30 on Sat 23rd
 //one more change from laptop to master to pull down to mac
+<<<<<<< Updated upstream
 // sue added newline at 1/25 at 714
 //Hatters robotics at 1034
 //changed code on KV Robotics computer
+=======
+//mod on mac to sync with other laptops
+>>>>>>> Stashed changes
