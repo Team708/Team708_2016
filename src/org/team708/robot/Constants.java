288798fc3ec79e48cd0cb5601037ed9c367ba07b
jpiergal <john.piergallini@lmco.com>
2016-02-12 20:55:58 -0500

    update commit

--- src/org/team708/robot/Constants.java
+++ src/org/team708/robot/Constants.java
@@ -24,8 +24,8 @@ public final class Constants {
 	public static final double MOTOR_REVERSE 		= .3;
 	public static final double MOTOR_OFF 			= 0.0;
 
-	public static final double DRIVE_MOTOR_MAX_SPEED 	= 0.4;
-	public static final double ROTATE_MOTOR_MAX_SPEED 	= 0.4;
+	public static final double DRIVE_MOTOR_MAX_SPEED 		= 0.4;
+	public static final double ROTATE_MOTOR_MAX_SPEED 		= 0.4;
 	public static final double ARMPIVOT_MOTOR_MAX_SPEED 	= 0.6;
 	
 
@@ -33,14 +33,14 @@ public final class Constants {
 	 * Smart Dashboard
 	 */
 	public static final double SEND_STATS_INTERVAL	= .5;	// Interval for reporting in seconds
-	public static final boolean DEBUG 		= false;
+	public static final boolean DEBUG 				= false;
 	
 
 	/*
 	 * Sensors
 	 */
-	public static final double IR_HAS_BALL_DISTANCE 			= 6.0;
-	public static final double ENCODER_BOTTOM_POSITION 			= 0.0;
+	public static final double IR_HAS_BALL_DISTANCE 					= 6.0;
+	public static final double ENCODER_BOTTOM_POSITION 					= 0.0;
 	public static final double GRAYHILL_ENCODER_PULSES_PER_REVOLUTION 	= 128.0;
 	
 	/*
@@ -53,14 +53,14 @@ public final class Constants {
 	 * PIVOT ARM -- moves arm up and down
 	 */
 	public static final double PIVOT_ARM_SPROCKET_DIAMETER 	= 1.8;
-	public static final double PIVOT_UP 			= 1.0;
-	public static final double PIVOT_DOWN 			= 0.25;
+	public static final double PIVOT_UP 					= 1.0;
+	public static final double PIVOT_DOWN 					= 0.25;
 	
 	
 	/*
 	 * GRAPPLER -- extends arm (telescoping)
 	 */
-	public static final double GRAPPLER_MOTOR_MINIMUM 	= 0.3;
+	public static final double GRAPPLER_MOTOR_MINIMUM 		= 0.3;
 	public static final double GRAPPLER_SPROCKET_DIAMETER 	= 1.4;
 
 	
@@ -74,11 +74,11 @@ public final class Constants {
 	/*
 	 * Drivetrain
 	 */
-	public static final double 	TANK_STICK_TOLERANCE 			= .30;
-	public static final double 	DRIVETRAIN_WHEEL_DIAMETER 		= 4.0;
+	public static final double 	TANK_STICK_TOLERANCE 				= .30;
+	public static final double 	DRIVETRAIN_WHEEL_DIAMETER 			= 4.0;
 	public static final double 	DRIVETRAIN_ENCODER_PULSES_PER_REV 	= 128.0;
-	public static final boolean DRIVE_USE_SQUARED_INPUT 		= false;
-	public static final boolean DRIVETRAIN_USE_LEFT_ENCODER		= true; // variable to determine which side encoder is on
+	public static final boolean DRIVE_USE_SQUARED_INPUT 			= false;
+	public static final boolean DRIVETRAIN_USE_LEFT_ENCODER			= true; // variable to determine which side encoder is on
 	
 	
 	// PID Tuning parameters
