af016fa630d663d9c8712d51bd77eb9b037c6f32
kv Robotics <kvrobotics@team708.org>
2016-02-21 17:27:07 -0500

    AutonomousConstants and the rive n shoot shttufff
    asifhisdigfidse

--- src/org/team708/robot/AutoConstants.java
+++ src/org/team708/robot/AutoConstants.java
@@ -7,13 +7,29 @@ public final class AutoConstants {
 	 */
 	public static final double NINETY_DEGREE_TURN 		= 85.0;
 	public static final double TURN_SPEED     		= 0.6;
+	public static final double TURN_LEFT     		= -1.0;
+	public static final double TURN_RIGHT     		= 1.0;
+	public static final double NEIN_TURN     		= 0.0;
+	
+	public static final double ROBOT_ENCODER_DRIVE_SPEED     		= 0.6;
+	public static final double ROBOT_TIME_DRIVE_SPEED	     		= 1.0;
 	
 	public static final double ROBOT_LENGTH 		= 31.0;
+	
+	public static final double SHOOTING_SONAR_DISTANCE     		= 44;
 	
 	/*
 	 * Robot To Defense
 	 */
+	public static final double GO_TO_1_DEFENSES     		= 1;
+	public static final double GO_TO_2_DEFENSES     		= 2;
+	public static final double GO_TO_NO_DEFENSES     		= 0;
+	
 	public static final double ROBOT_TO_DEFENSE_DISTANCE 	= 52.0;
 	public static final double ROBOT_TO_TARGET_DISTANCE 	= 52.0;
+	public static final double ROBOT_THROUGH_LOW_BAR		= 132.0;
+	
+	public static final double ROBOT_OVER_DEFENSE_TIME			= 3.0;
+	
 	
 }
---
+++ src/org/team708/robot/commands/autonomous/Drive2TurnRightShoot.java
@@ -0,0 +1,55 @@
+package org.team708.robot.commands.autonomous;
+import edu.wpi.first.wpilibj.Preferences;
+
+import org.team708.robot.AutoConstants;
+import org.team708.robot.Constants;
+import org.team708.robot.Robot;
+import org.team708.robot.commands.arm.ArmDown;
+import org.team708.robot.commands.arm.JoystickMoveArm;
+import org.team708.robot.commands.drivetrain.DriveStraightForTime;
+import org.team708.robot.commands.drivetrain.DriveStraightToEncoderDistance;
+import org.team708.robot.commands.drivetrain.RotateAndDriveToTarget;
+import org.team708.robot.commands.drivetrain.TurnToDegrees;
+import org.team708.robot.commands.shooter.AutoShoot;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+/**
+ *
+ */
+public class Drive2TurnRightShoot extends CommandGroup {
+	
+
+	
+	
+	public  Drive2TurnRightShoot(double turnDirection){
+		
+		addSequential(new ArmDown()); //always gonna happen
+		addSequential(new DriveStraightToEncoderDistance(AutoConstants.ROBOT_TO_DEFENSE_DISTANCE * AutoConstants.GO_TO_2_DEFENSES, AutoConstants.ROBOT_ENCODER_DRIVE_SPEED, false));
+		
+		addSequential(new TurnToDegrees(AutoConstants.TURN_SPEED, 90 * AutoConstants.TURN_RIGHT));
+		addSequential(new DriveStraightForTime(AutoConstants.ROBOT_TIME_DRIVE_SPEED, AutoConstants.ROBOT_OVER_DEFENSE_TIME));
+		addSequential(new RotateAndDriveToTarget(AutoConstants.SHOOTING_SONAR_DISTANCE));
+		addSequential(new AutoShoot());
+		
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+	
+	
+}
