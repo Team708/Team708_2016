8d12c3b56c01fa4156785c48e239d13a39d3d95a
Tom <tommyzhao111@gmail.com>
2016-04-01 18:00:35 -0400

    Added Low Goal Autonomous
    Added a low goal shot command that runs intake and loader out.

--- src/org/team708/robot/commands/intake/AutoLowGoal.java
+++ src/org/team708/robot/commands/intake/AutoLowGoal.java
@@ -1,48 +1,44 @@
-package org.team708.robot.commands.shooter;
+package org.team708.robot.commands.intake;
+
 import org.team708.robot.Constants;
 import org.team708.robot.OI;
 import org.team708.robot.Robot;
-import org.team708.robot.subsystems.Loader;
-import org.team708.robot.util.Gamepad;
-import org.team708.robot.commands.shooter.Fire;
+import org.team708.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
- *
+ *@author James_Makovics
+ *@author Alex Tysak
+ *@author Thomas Zhao
  */
-public class AutoStopSL extends Command {
+public class AutoLowGoal extends Command {
 
-
-    public AutoStopSL() {
+	
+    public AutoLowGoal() {
+    	requires(Robot.intake);
     	requires(Robot.loader);
-    	requires(Robot.shooter);
     }
     
+
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.loader.stop(); 	
-		Robot.shooter.stop();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-		Robot.loader.stop(); 	
-		Robot.shooter.stop();
-		Robot.intake.stop();
+    	Robot.loader.manualMove(Constants.LOADER_MOTOR_REVERSE);
+    	Robot.intake.moveMotor(Constants.INTAKE_REVERSE);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	return true;
+    	return(true);
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.loader.stop();
-    	Robot.shooter.stop();
-    	Robot.intake.stop();
     }
 
     // Called when another command which requires one or more of the same
